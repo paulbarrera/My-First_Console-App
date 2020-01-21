83f70b3dcf05b695422cb5368d935ed3186a4a7d
Paul Barrera <paul@paulbarrera.com>
2020-01-21 18:55:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_First_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

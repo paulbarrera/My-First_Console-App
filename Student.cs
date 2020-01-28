c3c5b4cadd0983c57abe39e09f14a1dd232286b2
Paul Barrera <paul@paulbarrera.com>
2020-01-28 18:34:31 -0500

    hello world

---
+++ Student.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace My_First_Console_App
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public string LastClassCompleted { get; set; }
+        public DateTimeOffset LastClassCompletedOn { get; set; }
+    }
+}

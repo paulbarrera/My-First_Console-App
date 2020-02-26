7892318ac9e57aae8c6c3ebca6d634da9a218f45
Paul Barrera <paul@paulbarrera.com>
2020-02-25 19:21:36 -0500

    added student tryparse validation

--- Program.cs
+++ Program.cs
@@ -1,21 +1,77 @@
 ﻿  
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace My_First_Console_App
 {
     class Program
     {
-         static void Main(string[] args)
+        static List<Student> studentsList = new List<Student>();
+        static void Main(string[] args)
         {
             var inputtingStudent = true;
-            while (inputtingStudent) {
-                InputStudent();
-                Console.WriteLine("Would you like to continue inputting students? [Y/N]");
-                inputtingStudent = Console.ReadLine().ToLower() == "y";
-            }
-        }
-        static Student InputStudent()
+            
+            while (inputtingStudent) 
         {
+            DisplayMenu();
+            var option = Console.ReadLine();
+            
+            switch (int.Parse(option))
+        {       
+            case 1:
+            InputStudent();
+            break;
+
+            case 2:
+            DisplayStudents();
+            break;
+
+            case 3:
+            SearchStudents();
+            break;
+
+            case 4:
+                inputtingStudent = false;
+            break;
+        }
+    }
+}
+            private static void DisplayStudents(IEnumerable<Student> students)
+            {
+             if (students.Any())
+                {   
+                Console.WriteLine($"Student Id | Name |  Class ");
+                studentsList.ForEach(x =>
+            {
+                Console.WriteLine("No students found.");
+            });
+        } 
+         else
+        {
+                System.Console.WriteLine("No students found.");
+        }        
+    } 
+            private static void DisplayStudents() => DisplayStudents(studentsList);                           
+            private static void SearchStudents()
+            {
+                Console.WriteLine("Search string:");
+                var searchString = Console.ReadLine();
+                var students = studentsList.Where(x => x.FullName.Contains(searchString)).ToList();
+                DisplayStudents(students);
+            }
+            private static void DisplayMenu()
+            {
+                Console.WriteLine("Select from the following operations:");
+                Console.WriteLine("1: Enter new student");
+                Console.WriteLine("2: List all students");
+                Console.WriteLine("3: Search for student by name");
+                Console.WriteLine("4: Exit");
+            }
+
+            static Student InputStudent()
+            {
+            
             Console.WriteLine("Enter Student Id");
             var studentId = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter First Name");
@@ -43,6 +99,6 @@ namespace My_First_Console_App
             Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} ");
             Console.ReadKey();    
             return studentRecord;
-        }
-}
+         }
+    }
 }
